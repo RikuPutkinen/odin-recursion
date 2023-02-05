5c6c1e713adea14d85e8e9ea6593e179687b27ed
Riku Putkinen <putkinenriku@gmail.com>
2023-02-05 02:25:31 +0200

    Add the recursive method

--- fibonacci.js
+++ fibonacci.js
@@ -10,3 +10,21 @@ function fibs(num) {
 }
 
 console.log(fibs(8));
+
+//recursion
+function fibsRec(num=0) {
+  if (num <= 0) return [];
+  if (num === 1) return [0];
+  if (num === 2) return [0, 1];
+
+  function fibsRecInner(num, arr=[0,1]) {
+    if (num === 1) return 0;
+    if (num === 2) return 1;
+    if (num > arr.length) fibsRecInner(num-1, arr);
+    arr.push(arr[arr.length - 2] + arr[arr.length - 1]);
+    return arr;
+  }
+  return fibsRecInner(num);
+}
+
+console.log(fibsRec(8));
